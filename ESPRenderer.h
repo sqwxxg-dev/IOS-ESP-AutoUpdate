3aebd66c7a699bf2b1736056eb64852ad33036fa
sqwxxg-dev <sqwxxg@gmail.com>
2026-08-25 17:37:53 +0300

    Update ESPRenderer.h

--- ESPRenderer.h
+++ ESPRenderer.h
@@ -1,11 +1,11 @@
 // ESPRenderer.h
 #import <UIKit/UIKit.h>
-#import <QuartzCore/QuartzCore.h>
 
 @interface ESPRenderer : NSObject
 
 + (instancetype)sharedInstance;
 - (void)setupOverlayWindow;
+- (UIWindow *)getESPWindow;
 - (void)renderESP:(void (^)(CGContextRef context))drawBlock;
 
 @end
